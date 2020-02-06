e6115260ef9bf7e2dd8d507d3b9e26a033a009d7
Henrik Hautakoski <henrik@eossweden.org>
2020-02-06 15:51:22 +0100

    eosapi/types.go: Adding Health and Service structs.

--- eosapi/types.go
+++ eosapi/types.go
@@ -1,11 +1,23 @@
 
 package eosapi;
 
-import "time";
+import "time"
 
 // get_info format (not all fields).
 type Info struct {
 	ServerVersion 	string 		`json:"server_version"`
 	HeadBlockNum 	int64 		`json:"head_block_num"`
 	HeadBlockTime 	time.Time 	`json:"head_block_time"`
+}
+
+type Service struct {
+	Name	string 				   `json:"service"`
+	Status  string  			   `json:"status"`
+	Data    map[string]interface{} `json:"service_data"`
+	Time    int64 				   `json:"time"` // unix timestamp.
+}
+
+type Health struct {
+	VersionHash 	string 		`json:"version_hash"`
+	Health          []Service 	`json:"health"`
 }
