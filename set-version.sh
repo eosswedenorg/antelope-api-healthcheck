4375afa4fec6f2ef1ee3033ede21c5aa6c3c0571
Henrik Hautakoski <henrik@eossweden.org>
2021-12-02 13:23:03 +0100

    Adding set-version.sh

---
+++ set-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "$0 <version>"
+	exit 1
+fi
+
+sed -i "s:PACKAGE_VERSION=\\\\\"\(.*\)\\\\\":PACKAGE_VERSION=\\\\\"$1\\\\\":g" Makefile
+sed -i "s~\print(\"Version:\ v\(.*\)\\\n\")~print(\\\"Version:\ v$1\\\n\\\")~g" src/main.go
