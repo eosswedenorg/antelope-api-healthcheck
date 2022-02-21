415b1346dc475475f5409600949bdb5910b7c2bb
Henrik Hautakoski <henrik@eossweden.org>
2022-02-21 13:43:00 +0100

    Adding src/api/eosio-contract.go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/eosswedenorg/eosio-api-healthcheck
 go 1.14
 
 require (
+	github.com/eosswedenorg-go/eos-contract-api-client v0.0.0-20220221105418-dc591fcc0dc5 // indirect
 	github.com/eosswedenorg-go/eosapi v0.1.0
 	github.com/eosswedenorg-go/haproxy v0.0.0-20220101140534-fccfdd93a8cd
 	github.com/eosswedenorg-go/pid v1.0.0
@@ -11,9 +12,10 @@ require (
 	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/pborman/getopt/v2 v2.1.0
+	internal/api v1.0.0
 	internal/utils v1.0.0
-    internal/api v1.0.0
 )
 
 replace internal/utils => ./src/utils
+
 replace internal/api => ./src/api
---
+++ src/api/eosio-contract.go
@@ -0,0 +1,74 @@
+
+package api
+
+import (
+    "fmt"
+    "time"
+    "github.com/eosswedenorg-go/haproxy"
+    contract_api "github.com/eosswedenorg-go/eos-contract-api-client"
+)
+
+type EosioContract struct {
+    client contract_api.Client
+    block_time float64
+}
+
+func NewEosioContract(url string, block_time float64) EosioContract {
+    return EosioContract{
+        client: contract_api.Client{
+            Url: url,
+        },
+        block_time: block_time,
+    }
+}
+
+func (e EosioContract) LogInfo() LogParams {
+    return LogParams{
+        "type", "eosio-contract",
+        "url", e.client.Url,
+        "block_time", e.block_time,
+    }
+}
+
+//  check_api - Validates head block time.
+// ---------------------------------------------------------
+func (e EosioContract) Call() (haproxy.HealthCheckStatus, string) {
+
+    h, err := e.client.GetHealth()
+    if err != nil {
+        msg := fmt.Sprintf("%s", err);
+        return haproxy.HealthCheckFailed, msg
+    }
+
+    // Check HTTP Status Code
+    if h.HTTPStatusCode > 299 {
+        return haproxy.HealthCheckDown,
+            fmt.Sprintf("Taking offline because %v was received from backend", h.HTTPStatusCode)
+    }
+
+    // Check postgres
+    if h.Data.Postgres.Status != "OK" {
+        return haproxy.HealthCheckDown,
+            fmt.Sprintf("Taking offline because Postgres reported '%s'", h.Data.Postgres.Status)
+    }
+
+    // Check redis
+    if h.Data.Redis.Status != "OK" {
+        return haproxy.HealthCheckDown,
+            fmt.Sprintf("Taking offline because Redis reported '%s'", h.Data.Postgres.Status)
+    }
+
+    // Validate head block.
+    now  := time.Now().In(time.UTC)
+    diff := now.Sub(h.Data.Chain.HeadTime).Seconds()
+
+    if diff > e.block_time {
+        return haproxy.HealthCheckDown,
+            fmt.Sprintf("Taking offline because head block is lagging %.0f seconds", diff)
+    } else if diff < -e.block_time {
+        return haproxy.HealthCheckDown,
+            fmt.Sprintf("Taking offline because head block is %.0f seconds into the future", diff)
+    }
+
+    return haproxy.HealthCheckUp, "OK"
+}
