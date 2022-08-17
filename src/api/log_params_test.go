816a38acae76452c4e68b9358dafdbcb1fa1ec9e
Henrik Hautakoski <henrik@eossweden.org>
2022-08-17 16:55:22 +0200

    Adding src/api/log_params_test.go

---
+++ src/api/log_params_test.go
@@ -0,0 +1,34 @@
+
+package api
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+
+
+func TestLogParams(t *testing.T) {
+
+    type test_struct struct {
+        First string
+        Second int
+    }
+
+    p := LogParams{}
+
+    p.Add("one", 1)
+    p.Add("string", "str")
+    p.Add("struct", test_struct{First:"first_string",Second:1234})
+
+    expected := []interface{}([]interface {}{
+        "one",1,
+        "string","str",
+        "struct",test_struct{
+            First:"first_string",
+            Second:1234,
+        },
+    })
+
+    assert.Equal(t, expected, p.ToSlice())
+}
