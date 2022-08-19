4a7d74594e1871576adea698691127596e92ff4e
Henrik Hautakoski <henrik@eossweden.org>
2022-08-19 13:21:59 +0200

    set-version.sh: update version in pkg_info.sh instead of Makefile

--- set-version.sh
+++ set-version.sh
@@ -5,5 +5,5 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-sed -i "s:PACKAGE_VERSION=\\\\\"\(.*\)\\\\\":PACKAGE_VERSION=\\\\\"$1\\\\\":g" Makefile
+sed -i "s:PROGRAM_VERSION=\(.*\):PROGRAM_VERSION=$1:g" scripts/info.sh
 sed -i "s~\print(\"Version:\ v\(.*\)\\\n\")~print(\\\"Version:\ v$1\\\n\\\")~g" src/main.go
