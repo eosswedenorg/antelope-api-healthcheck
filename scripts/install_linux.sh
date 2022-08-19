1f2252b2b534b82c840fd4cd28b62efb913b5eac
Henrik Hautakoski <henrik@eossweden.org>
2022-08-19 13:31:15 +0200

    scripts/install_linux.sh: call log_install for each file.

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Script to install program files on linux systems
 
+source ${BASE_DIR}/functions/log_install.sh
+
 SYSUNITDIR=${DESTDIR}/etc/systemd/system
 RSYSLOGDIR=${DESTDIR}/etc/rsyslog.d
 LOGROTATEDIR=${DESTDIR}/etc/logrotate.d
 
 # Create service file
+log_install ${SYSUNITDIR}/${PROGRAM_NAME}.service
 mkdir -p ${SYSUNITDIR}
 cat ${TEMPLATE_DIR}/sysunit.service \
     | sed "s~{{ PROGRAM_NAME }}~${PROGRAM_NAME}~" \
@@ -14,12 +17,14 @@ cat ${TEMPLATE_DIR}/sysunit.service \
     > ${SYSUNITDIR}/${PROGRAM_NAME}.service
 
 # Create systemd config file
+log_install ${DESTDIR}${ETCDIR}/env
 mkdir -p ${DESTDIR}${ETCDIR}
 cat ${TEMPLATE_DIR}/config \
     | sed "s~{{ PROGRAM_NAME }}~${PROGRAM_NAME}~" \
     > ${DESTDIR}${ETCDIR}/env
 
 # Create rsyslog file
+log_install ${RSYSLOGDIR}/49-${PROGRAM_NAME}.conf
 mkdir -p ${RSYSLOGDIR}
 cat ${TEMPLATE_DIR}/rsyslog.conf \
     | sed "s~{{ PROGRAM }}~${PROGRAM_NAME}~" \
@@ -27,6 +32,7 @@ cat ${TEMPLATE_DIR}/rsyslog.conf \
     > ${RSYSLOGDIR}/49-${PROGRAM_NAME}.conf
 
 # Create logrotate file
+log_install ${LOGROTATEDIR}/${PROGRAM_NAME}.conf
 mkdir -p ${LOGROTATEDIR}
 cat ${TEMPLATE_DIR}/logrotate.conf \
     | sed "s~{{ LOG_FILE }}~${LOGFILE}~" \
@@ -34,6 +40,7 @@ cat ${TEMPLATE_DIR}/logrotate.conf \
 chmod 644 ${LOGROTATEDIR}/${PROGRAM_NAME}.conf
 
 # Copy program
+log_install ${DESTDIR}${SHAREDIR}
 mkdir -p ${DESTDIR}/${BINDIR}
 cp ${BUILD_DIR}/${PROGRAM_NAME} ${DESTDIR}${BINDIR}/${PROGRAM_NAME}
 
