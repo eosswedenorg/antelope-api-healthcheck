3568e7c908c4d81b2bc72463ce0b111e1ddb833b
Henrik Hautakoski <henrik@eossweden.org>
2022-10-21 13:14:23 +0200

    src/utils/parse_log_formatter_test.go: fix whitespace.

--- src/utils/parse_log_formatter_test.go
+++ src/utils/parse_log_formatter_test.go
@@ -2,28 +2,28 @@ package utils
 
 import (
     "reflect"
-	"testing"
+    "testing"
 
-	log "github.com/inconshreveable/log15"
+    log "github.com/inconshreveable/log15"
 )
 
 func Test_ParseLogFormatter(t *testing.T) {
-	tests := []struct {
-		name string
-		arg  string
-		want log.Format
-	}{
-		{ "Default", "", log.TerminalFormat() },
+    tests := []struct {
+        name string
+        arg  string
+        want log.Format
+    }{
+        { "Default", "", log.TerminalFormat() },
         { "LogFmt", "logfmt", log.LogfmtFormat() },
         { "Json", "json", log.JsonFormat() },
         { "JsonPretty", "json-pretty", log.JsonFormat() },
         { "Unknown", "unknown", log.TerminalFormat() },
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ParseLogFormatter(tt.arg); reflect.ValueOf(got).Pointer() != reflect.ValueOf(tt.want).Pointer() {
-				t.Errorf("parseLogFormatter() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := ParseLogFormatter(tt.arg); reflect.ValueOf(got).Pointer() != reflect.ValueOf(tt.want).Pointer() {
+                t.Errorf("parseLogFormatter() = %v, want %v", got, tt.want)
+            }
+        })
+    }
 }
